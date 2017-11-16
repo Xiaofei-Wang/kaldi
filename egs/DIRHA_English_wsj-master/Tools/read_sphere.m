1c14baf34a355e20dead3fd2701b732cd773e40c
xiaofei-wang <xiaofei.wang1987@gmail.com>
2017-11-16 13:46:09 -0500

    delete some files

--- egs/DIRHA_English_wsj-master/Tools/read_sphere.m
+++
@@ -1,8 +0,0 @@
-function [y]=read_sphere(x)
-cmd=strcat('./w_decode -f -o short_01$',  x , '$tmp.wav');
-cmd=strrep(cmd,'$',' ');
-system(cmd);
-fp=fopen('tmp.wav','r');
-[y,n]=fread(fp,Inf,'short');
-fclose(fp);
-y=y(513:end);
