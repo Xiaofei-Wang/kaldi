0d7d7690b385649a445427353a279afb043027c0
xiaofei-wang <xiaofei.wang1987@gmail.com>
2018-03-28 11:42:42 -0400

    revise get_oracle.sh

--- egs/chime5/s5_beam/local/get_oracle.sh
+++ egs/chime5/s5_beam/local/get_oracle.sh
@@ -15,9 +15,11 @@ min_lmwt=7
 max_lmwt=17
 stage=0
 
-filtfiledir=exp/chain_train_worn_u100k_cleaned/tdnn1a_sp/decode_dev_beamformit
-dir=exp/chain_train_worn_u100k_cleaned/tdnn1a_sp/decode_dev_beamformit_u12346_oracle
-sdir=exp/chain_train_worn_u100k_cleaned/tdnn1a_sp/decode_dev_beamformit
+sdir=$1
+dir=$2
+
+#dir=exp/chain_train_worn_u100k_cleaned/tdnn1a_sp/decode_dev_beamformit_u12346_oracle
+#sdir=exp/chain_train_worn_u100k_cleaned/tdnn1a_sp/decode_dev_beamformit
 
 mkdir -p $dir
 
@@ -28,7 +30,7 @@ for mic in u01 u02 u03 u04 u06; do
     while read line; do
         i1=`echo ${line} | awk '{print $1}'`
         echo ${line} > $dir/test_filt_${i1}.txt	    
-      
+     
 	for wip in $(echo $word_ins_penalty | sed 's/,/ /g');do
           mkdir -p $dir/dev_beamformit_$mic/scoring_kaldi/penalty_$wip/log
 	  mkdir -p $dir/$wip/dev_beamformit_$mic
@@ -40,39 +42,55 @@ for mic in u01 u02 u03 u04 u06; do
          done
 	 rm $dir/test_filt_${i1}.txt
 	 num=$(($num+1));
-    done < ${filtfiledir}_$mic/scoring_kaldi/test_filt.txt
-
-
-#    for wip in $(echo $word_ins_penalty | sed 's/,/ /g'); do
-#	for lmwt in $(seq $min_lmwt $max_lmwt); do
-#	    for mic in L1C L4L LD07 L3L L2R Beam_Circular_Array; do
-#		grep WER $dir/$wip/$mic/wer_${lmwt}_${wip}_${i1}_${mic} /dev/null
-#	    done | utils/best_wer.sh >& $dir/$wip/best_wer_${lmwt}_${i1} || exit 1
-#	    best_wer_file=$(awk '{print $NF}' $dir/$wip/best_wer_${lmwt}_${wip}_${i1})
-#	    echo $best_wer_file
-#	    best_mic=$(echo $best_wer_file | awk -F_ '{print $NF}')
-#	    if [ $best_mic = 'Array' ]; then
-#		best_mic_temp=$(echo $best_wer_file | awk -F_ '{N=NF-1; print $N}')
-#		best_mic=Beam_${best_mic_temp}_Array
-#	    fi
-#	    echo $best_mic
-#	    mkdir -p $dir/scoring_kaldi/penalty_$wip
-#	    grep ${i1} ${sdir}_${best_mic}/scoring_kaldi/penalty_$wip/${lmwt}.txt >> $dir/scoring_kaldi/penalty_$wip/${lmwt}.txt
-#	done
-#    done
-
-
+    done < ${sdir}_$mic/scoring_kaldi/test_filt.txt
 done
+
+for num in `seq 1 7437`; do 
+    for wip in $(echo $word_ins_penalty | sed 's/,/ /g'); do
+	for lmwt in $(seq $min_lmwt $max_lmwt); do
+	    for mic in u01 u02 u03 u04 u06; do
+		grep WER $dir/$wip/dev_beamformit_$mic/wer_${lmwt}_${wip}_dev_beamformit_${mic}_${num} /dev/null
+	    done | utils/best_wer.sh >& $dir/$wip/best_wer_${lmwt}_${wip}_${num} || exit 1
+	    best_wer_file=$(awk '{print $NF}' $dir/$wip/best_wer_${lmwt}_${wip}_${num})
+	    echo $best_wer_file
+	    best_mic=$(echo $best_wer_file | awk -F_ '{N=NF-1; print $N}')
+	    echo $best_mic
+	    mkdir -p $dir/scoring_kaldi/penalty_$wip
+	    sed -n ${num}p ${sdir}_${best_mic}/scoring_kaldi/penalty_$wip/${lmwt}.txt >> $dir/scoring_kaldi/penalty_$wip/${lmwt}.txt
+
+	done
+    done
+done
+
 
 fi
 
-#cp $filtfiledir/test_filt.txt $dir/scoring_kaldi/test_filt.txt 
+cp ${sdir}_u01/scoring_kaldi/test_filt.txt $dir/scoring_kaldi/test_filt.txt 
 
 #for wip in $(echo $word_ins_penalty | sed 's/,/ /g'); do
 #    mkdir -p $dir/scoring_kaldi/penalty_$wip/log
 #   $cmd LMWT=$min_lmwt:$max_lmwt $dir/scoring_kaldi/penalty_$wip/log/score.LMWT.log \
-#      cat $dir/scoring_kaldi/penalty_$wip/LMWT.txt \| \
-#      compute-wer --text --mode=present \
-#      ark:$dir/scoring_kaldi/test_filt.txt  ark,p:- ">&" $dir/wer_LMWT_$wip || exit 1;
+#	      cat $dir/scoring_kaldi/penalty_$wip/LMWT.txt \| \
+#	      compute-wer --text --mode=present \
+#	      ark:$dir/scoring_kaldi/test_filt.txt  ark,p:- ">&" $dir/wer_LMWT_$wip || exit 1;
 #done
+
+for wip in $(echo $word_ins_penalty | sed 's/,/ /g'); do
+
+      for lmwt in $(seq $min_lmwt $max_lmwt); do
+
+	  awk '{print $1}' $dir/scoring_kaldi/penalty_$wip/${lmwt}.txt > $dir/scoring_kaldi/p.txt
+          awk '{$1="";print $0}' $dir/scoring_kaldi/test_filt.txt > $dir/scoring_kaldi/q.txt
+	  paste $dir/scoring_kaldi/p.txt $dir/scoring_kaldi/q.txt > $dir/scoring_kaldi/pq.txt
+
+	  $cmd  $dir/scoring_kaldi/penalty_$wip/log/score.${lmwt}.log \
+          	cat $dir/scoring_kaldi/penalty_$wip/${lmwt}.txt \| \
+                compute-wer --text --mode=present \
+                ark:$dir/scoring_kaldi/pq.txt  ark,p:-  ">&" $dir/wer_${lmwt}_$wip || exit 1;
+
+     done
+done
+rm $dir/scoring_kaldi/p.txt
+rm $dir/scoring_kaldi/q.txt
+rm $dir/scoring_kaldi/pq.txt
 
